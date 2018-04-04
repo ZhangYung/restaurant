5acf2e7f3c0e6df11c441e738e0c40987fe670b6
ZhangYaoYuan <Zhang_Yung@126.com>
2018-04-04 11:40:08 +0800

    update

--- index.php
+++ index.php
@@ -5,24 +5,25 @@
 <title>restaurant</title>
 </head>
 <body>
-<!-- ?php
-	$dbhost = 'localhost';
-	$dbuser = 'root';
-	$dbpass = 'ZhangYaoYuan@529143'
-	$conn = mysqli_connect($dbhost, $dbuser, $dbpass);
-	if (!$conn) {
-		die('error:' . mysqli_error($conn));
- 	}
- 	echo 'successful';
- 	$sql = 'create database RUNOOBTEST';
- 	$retval = mysqli_query($conn, $sql);
- 	if (!$retval) {
- 		die('create error:' . mysqli_error($conn));
-  	}
+<?php
+echo "hello word";
+// 	$dbhost = 'localhost';
+// 	$dbuser = 'root';
+// 	$dbpass = 'ZhangYaoYuan@529143'
+// 	$conn = mysqli_connect($dbhost, $dbuser, $dbpass);
+// 	if (!$conn) {
+// 		die('error:' . mysqli_error($conn));
+//  	}
+//  	echo 'successful';
+//  	$sql = 'create database RUNOOBTEST';
+//  	$retval = mysqli_query($conn, $sql);
+//  	if (!$retval) {
+//  		die('create error:' . mysqli_error($conn));
+//   	}
 
-  	echo "create success  \n";
-  	mysqli_close($conn);
- ? -->
+//   	echo "create success  \n";
+//   	mysqli_close($conn);
+ ?>
 <h1>我的第一个标题</h1>
 <p>我的第一个段落。</p>
 
