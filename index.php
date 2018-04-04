b5ae53a80faa0a1d229fb04f419ba9ec81dd5457
ZhangYaoYuan <Zhang_Yung@126.com>
2018-04-04 14:45:40 +0800

    update

--- index.php
+++ index.php
@@ -6,15 +6,34 @@
 </head>
 <body>
 <?php
-$name = "runoob";
-$a = <<<EOF
- <h1>我的第一个标题</h1>
-    <p>我的第一个段落。</p>
-	"adb"$name
-	"1223"
-EOF;
-echo $a;
- ?>
+class Car
+{
+    var $color;
+    function Car($color="green") {
+      $this->color = $color;
+    }
+    function what_color() {
+      return $this->color;
+    }
+}
+
+function print_vars($obj) {
+   foreach (get_object_vars($obj) as $prop => $val) {
+     echo "\t$prop = $val\n";
+   }
+}
+
+// 实例一个对象
+$herbie = new Car("white");
+
+// 显示 herbie 属性
+echo "herbie: Properties\n";
+print_vars($herbie);
+
+echo "herbie: Properties yung)\n";
+
+?>  
+
 <h1>我的第一个标题</h1>
 <p>我的第一个段落。</p>
 
