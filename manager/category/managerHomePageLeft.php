dad0c3dec44cb4c4df4e5f1d02d1045a183bec82
ZhangYaoYuan <Zhang_Yung@126.com>
2018-04-08 18:18:29 +0800

    update

--- manager/category/managerHomePageLeft.php
+++ manager/category/managerHomePageLeft.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<link rel="stylesheet" type="text/css" href="../stylesheet/homepage.css">
+	<link rel="stylesheet" type="text/css" href="../stylesheet/managerHomepage.css">
 </head>
 <body>
 	<br><br>
