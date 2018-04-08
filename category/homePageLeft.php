eb7f1a6076819eff49822fc8c7bb47a9ba68bdd7
ZhangYaoYuan <Zhang_Yung@126.com>
2018-04-08 15:10:37 +0800

    update

--- category/homePageLeft.php
+++ category/homePageLeft.php
@@ -1,11 +1,13 @@
 <!DOCTYPE html>
 <html>
 <head>
+	<link rel="stylesheet" type="text/css" href="StyleSheet/homepage.css">
 </head>
 <body>
-	<a href="category/fastFood.php" target="mainContent"> 美味快餐 </a><br><br>
-	<a href="category/hotPot.php" target="mainContent"> 火锅 </a><br><br>
-	<a href="category/drinks.php" target="mainContent"> 饮料 </a><br><br>
-	<a href="category/others.php" target="mainContent"> 其他 </a>
+	<br><br>
+	<a class="homePageSelectTitle" href="category/fastFood.php" target="mainContent"> 美味快餐 </a><br><br>
+	<a class="homePageSelectTitle" href="category/hotPot.php" target="mainContent"> 火锅 </a><br><br>
+	<a class="homePageSelectTitle" href="category/drinks.php" target="mainContent"> 饮料 </a><br><br>
+	<a class="homePageSelectTitle" href="category/others.php" target="mainContent"> 其他 </a>
 </body>
 </html>
