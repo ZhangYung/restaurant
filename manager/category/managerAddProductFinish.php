0ed45dc53781d06511e4e877116c0ee0b4a1ce04
ZhangYaoYuan <Zhang_Yung@126.com>
2018-04-08 17:04:45 +0800

    update

--- manager/category/managerAddProductFinish.php
+++ manager/category/managerAddProductFinish.php
@@ -1,5 +1,20 @@
-
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="../stylesheet/managerHomePage.css">
+</head>
 <?php 
 	echo $_POST["titleName"];
 	echo $_POST["price"];
+	echo $_POST["addType"];
+
  ?>
+<body>
+	<p>添加成功</p>
+	<br>
+	<br>
+	<form action="managerFastFood.php" method="get">
+		<button class="managerAdd" type="submit" name="addType" value="drinks"> 返回 </button> 
+	</form>
+</body>
+</html>
