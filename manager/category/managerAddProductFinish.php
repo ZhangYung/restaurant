4b56d9d0e8a1c7d613b05d6cd6bcc0accd75519a
ZhangYaoYuan <Zhang_Yung@126.com>
2018-04-08 18:29:32 +0800

    update

--- manager/category/managerAddProductFinish.php
+++ manager/category/managerAddProductFinish.php
@@ -8,31 +8,31 @@
 //config.php  数据库登录配置，就不共享出来了。
 echo $_SERVER['DOCUMENT_ROOT'] . "/restaurantConfig/config.php";
 	require $_SERVER['DOCUMENT_ROOT'] . "/restaurantConfig/config.php";
-	$fileFoldPath = $_SERVER['DOCUMENT_ROOT'] . "/restaurant/uploadProductImage/"
-
+	$fileFoldPath = $_SERVER['DOCUMENT_ROOT'] . "/restaurant/uploadProductImage/";
+	echo $fileFoldPath;
 	echo $mysqlServer;
 	echo $mysqlUsername;
 	echo $mysqlPassword;
 //连接mysql  请自行配置数据库$mysqlServer, $mysqlUsername, $mysqlPassword
-	// $conn = mysql_connect($mysqlServer, $mysqlUsername, $mysqlPassword);
-	// if (!$conn) {
-	// 	die("数据库连接失败，请联系管理员。");
-	// }
+	$conn = mysql_connect($mysqlServer, $mysqlUsername, $mysqlPassword);
+	if (!$conn) {
+		die("数据库连接失败，请联系管理员。");
+	}
 
 	echo "判断数据库是否存在pre";
-	// $databaseName = "meimeishop";
-	// $result = $conn->query("show databases like '" . $databaseName . "'");
-	// $row = $result->fetchAll();
-	// echo "判断数据库是否存在";
-	// echo $row;
-	// if (count($row) < '1') {
-	// //创建数据库
-	// 	if ($conn->query("create database " . $databaseName) === TRUE) {
-	// 		echo "创建数据库";
-	// 	 } else {
-	// 	 	echo "创建数据库失败";
-	// 	 }
-	// }
+	$databaseName = "meimeishop";
+	$result = $conn->query("show databases like '" . $databaseName . "'");
+	$row = $result->fetchAll();
+	echo "判断数据库是否存在";
+	echo $row;
+	if (count($row) < '1') {
+	//创建数据库
+		if ($conn->query("create database " . $databaseName) === TRUE) {
+			echo "创建数据库";
+		 } else {
+		 	echo "创建数据库失败";
+		 }
+	}
 
 
 	echo $mysqlServer;
