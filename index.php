706480110439100e7324ca1901c5b7f3cf134a0d
ZhangYaoYuan <Zhang_Yung@126.com>
2018-04-08 11:54:45 +0800

    update

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
 ?>  
 
 <div class="mui-content shortcut"> 
-	<a class="mui-control-item mui-active" ng-class="{'mui-active':lastTab===0}" data-tab="0">日用品</a>>
+	<a class="mui-control-item mui-active" ng-class="{'mui-active':lastTab===0}" data-tab="0">日用品</a>
 	<a class="mui-control-item" ng-class="{'mui-active':lastTab===1" data-tab="1">生活用品</a>
 	<a class="mui-control-item" ng-class="{'mui-active':lastTab===2" data-tab="2">数码产品</a>
 </div>
