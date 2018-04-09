1fc6baf518385512dc43e7f46e72e4bceeb98ea8
ZhangYaoYuan <Zhang_Yung@126.com>
2018-04-09 09:56:27 +0800

    update

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
 <link rel="stylesheet" type="text/css" href="stylesheet/homepage.css" />
 <title>水云居</title>
 </head>
+<body>
 <?php 
 //config.php  数据库登录配置，就不共享出来了。
 echo $_SERVER['DOCUMENT_ROOT'] . "/restaurantConfig/config.php" . "<br>"; 
@@ -14,11 +15,12 @@ echo $_SERVER['DOCUMENT_ROOT'] . "/restaurantConfig/config.php" . "<br>";
 	echo $mysqlServer . "<br>";
 	echo $mysqlUsername . "<br>";
 	echo $mysqlPassword . "<br>";
-// //连接mysql  请自行配置数据库$mysqlServer, $mysqlUsername, $mysqlPassword
-// 	$conn = mysql_connect($mysqlServer, $mysqlUsername, $mysqlPassword);
-// 	if (!$conn) {
-// 		die("数据库连接失败，请联系管理员。");
-// 	}
+//连接mysql  请自行配置数据库$mysqlServer, $mysqlUsername, $mysqlPassword
+	$conn = mysql_connect($mysqlServer, $mysqlUsername, $mysqlPassword);
+	if (!$conn) {
+		echo "失败";
+		die("数据库连接失败，请联系管理员。");
+	}
 
 	echo "判断数据库是否存在pre";
 	// $databaseName = "meimeishop";
@@ -41,7 +43,7 @@ echo $_SERVER['DOCUMENT_ROOT'] . "/restaurantConfig/config.php" . "<br>";
 	<frame name="selectContent" src="category/homePageLeft.php" noresize="noresize"></frame>
 	<frame name="mainContent" src="category/fastFood.php"></frame>
 </frameset>
-
+</body>
 </html>
 
 
