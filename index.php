e06d50e860bde9308d76ae26ead05e13f6baa360
ZhangYaoYuan <Zhang_Yung@126.com>
2018-04-09 14:12:35 +0800

    Update

--- index.php
+++ index.php
@@ -28,7 +28,8 @@ echo $_SERVER['DOCUMENT_ROOT'] . "/restaurantConfig/config.php" . "<br>";
 	$result = $conn->query($sql);
 	var_dump($result);
 	echo "<br>row:";
-	var_dump($result["num_rows"]);
+	var_dump($result->fetch_row());
+	
 	// echo "判断创建数据库"
 	// if ($result->num_rows() == 0) {
 	// //创建数据库
