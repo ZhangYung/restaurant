8956b6263ec7cb834c20741fcdc58a3200f933f0
ZhangYaoYuan <Zhang_Yung@126.com>
2018-04-09 14:19:35 +0800

    UPdate

--- index.php
+++ index.php
@@ -30,7 +30,7 @@ echo $_SERVER['DOCUMENT_ROOT'] . "/restaurantConfig/config.php" . "<br>";
 	echo "<br>row:";
 	var_dump($result->fetch_row());
 	echo "<br>count:";
-	var_dump(count($result->fetch_row());
+	var_dump(count($result->fetch_row()));
 
 	if (count($result->fetch_row()) == 0) {
 	//创建数据库
