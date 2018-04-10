2e2586dd0d2f970b002be7e7fdb429c5b5201967
ZhangYaoYuan <Zhang_Yung@126.com>
2018-04-10 17:08:43 +0800

    update

--- manager/category/managerAddProduct.php
+++ manager/category/managerAddProduct.php
@@ -7,11 +7,11 @@
 	<br>
 	<form action="managerAddProductFinish.php" method="POST" enctype="multipart/form-data">
 	<input type="hidden" name="addType" value=<?php echo $_GET["addType"];?> >
-	商品图片: <input type="file" name="image"><br>
-	商品名称： <input type="text" name="titleName"><br>
-	商品描述： <input type="text" name="detail"><br>
-	商品价格： <input type="number" name="price" step="0.01" min="0.01" max="999999" /><br>
-	商品数量： <input type="number" name="number" step="1" min="1" max="9999999" value="9999"><br>
+	商品图片:(文件要少于150kb) <input type="file" name="image"><br>
+	商品名称：<input type="text" name="titleName"><br>
+	商品描述：<input type="text" name="detail"><br>
+	商品价格：<input type="number" name="price" step="0.01" min="0.01" max="999999" /><br>
+	商品数量：<input type="number" name="number" step="1" min="1" max="9999999" value="9999"><br>
 
 	<input type="submit" name="submit" value="提交">
 	</form>
--- manager/category/managerAddProductFinish.php
+++ manager/category/managerAddProductFinish.php
@@ -15,23 +15,58 @@
 	$price = $_POST["price"];
 	$number = $_POST["number"];
 
-	// $uploadImageFileFoldPath;
- // 1:快餐fastFood 2:火锅hotPot 3:饮料drinks 4:其他others
-addOrEditProduct(NULL, $titleName, $price, $imagePath, $detail, $number, $addType, 1);
+	// 允许上传的图片后缀
+	$allowedExts = array("gif", "jpeg", "jpg", "png");
+	$temp = explode(".", $_FILES["file"]["name"]);
+	$extension = end($temp);     // 获取文件后缀名
 
-$productCategory = "快餐";
-$finishPage = "managerFastFood.php";
-if ($addType == 2) {
-	$finishPage = "managerHotPot.php";
-	$productCategory = "火锅";
-} elseif ($addType == 3) {
-	$finishPage = "managerDrinks.php";
-	$productCategory = "饮料";
-} elseif ($addType == 4) {
-	$finishPage = "managerOthers.php";
-	$productCategory = "其他";
-}
+	$uploadPath = $uploadImageFileFoldPath . intval(time()) . $_FILES["file"]["name"];
+	if ((($_FILES["file"]["type"] == "image/gif")
+	|| ($_FILES["file"]["type"] == "image/jpeg")
+	|| ($_FILES["file"]["type"] == "image/jpg")
+	|| ($_FILES["file"]["type"] == "image/pjpeg")
+	|| ($_FILES["file"]["type"] == "image/x-png")
+	|| ($_FILES["file"]["type"] == "image/png"))
+	&& ($_FILES["file"]["size"] < 307200)   // 小于 200 kb
+	&& in_array($extension, $allowedExts))
+	{
+	    if ($_FILES["file"]["error"] > 0)
+	    {
+	        echo "上传文件错误：: " . $_FILES["file"]["error"] . "<br>";
+	    }
+	    else
+	    {   
+	        if (file_exists($uploadPath))
+	        {
+	            echo $uploadPath . " 文件已经存在。如果多次尝试都有此问题，请联系管理员";
+	        }
+	        else
+	        {
+	            // 如果 upload 目录不存在该文件则将文件上传到 upload 目录下
+	            move_uploaded_file($_FILES["file"]["tmp_name"], $uploadPath);
+	        }
+	    }
+	}
+	else
+	{
+	    die "非法的文件格式, 文件只支持png,jpg,jpeg,文件要少于200kb";
+	}
 
+	// 1:快餐fastFood 2:火锅hotPot 3:饮料drinks 4:其他others
+	addOrEditProduct(NULL, $titleName, $price, $imagePath, $detail, $number, $addType, 1);
+
+	$productCategory = "快餐";
+	$finishPage = "managerFastFood.php";
+	if ($addType == 2) {
+		$finishPage = "managerHotPot.php";
+		$productCategory = "火锅";
+	} elseif ($addType == 3) {
+		$finishPage = "managerDrinks.php";
+		$productCategory = "饮料";
+	} elseif ($addType == 4) {
+		$finishPage = "managerOthers.php";
+		$productCategory = "其他";
+	}
  ?>
 	<p>添加成功 <?php echo $productCategory; ?> </p>
 	<br>
