f351ec120d98455e5778a501e85f2783dc8c70ce
ZhangYaoYuan <Zhang_Yung@126.com>
2018-04-10 13:48:19 +0800

    update

--- index.php
+++ index.php
@@ -12,11 +12,14 @@ require $_SERVER['DOCUMENT_ROOT'] . "/restaurantConfig/config.php";
 require $databaseManagerphpPath;
 echo $databaseManagerphpPath;
 
-checkAndCreateTable($meimeiShopOrderTable);
-checkAndCreateTable($meimeiShopProductTable);
+getProducts(NULL, NULL, NULL);
+getOrders(NULL, NULL, NULL);
 
-addOrEditProduct(NULL, "炒饭", 10.50, "http://www.baidu.com/dsa.png", "美味的黄金炒饭", 9999, 1, 1);
-addOrEditOrder(NULL, 3, intval(time()), 100, "1,2,3", "炒饭,可乐,鸡汤", "10.50,3,15", "2,1,3", 1);
+// checkAndCreateTable($meimeiShopOrderTable);
+// checkAndCreateTable($meimeiShopProductTable);
+
+// addOrEditProduct(NULL, "炒饭", 10.50, "http://www.baidu.com/dsa.png", "美味的黄金炒饭", 9999, 1, 1);
+// addOrEditOrder(NULL, 3, intval(time()), 100, "1,2,3", "炒饭,可乐,鸡汤", "10.50,3,15", "2,1,3", 1);
 
 ?>
 
