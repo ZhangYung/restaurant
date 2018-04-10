d121311c04f0e630fdac2726d04137308e670bca
ZhangYaoYuan <Zhang_Yung@126.com>
2018-04-10 16:28:46 +0800

    update

--- manager/category/managerAddProductFinish.php
+++ manager/category/managerAddProductFinish.php
@@ -6,27 +6,27 @@
 </head>
 <body>
 <?php 
-// $addType = $_POST("addType");
-$titleName = $_POST("titleName");
-// $price = $_POST("price");
-// echo "addType:";
-echo $titleName;
+	require $_SERVER['DOCUMENT_ROOT'] . "/restaurantConfig/config.php";
+	require $databaseManagerphpPath;
+	
+	$addType = $_POST["addType"];
+	$titleName = $_POST["titleName"];
+	$price = $_POST["price"];
+	echo "addType:";
+	echo $titleName;
 
-// 	require $_SERVER['DOCUMENT_ROOT'] . "/restaurantConfig/config.php";
-// 	require $databaseManagerphpPath;
-
-// 	// $uploadImageFileFoldPath;
-//  // 1:快餐fastFood 2:火锅hotPot 3:饮料drinks 4:其他others
-// // addOrEditProduct($productId, $name, $price, $imagePath, $detail, $number, $category, $state);
+	// $uploadImageFileFoldPath;
+ // 1:快餐fastFood 2:火锅hotPot 3:饮料drinks 4:其他others
+// addOrEditProduct($productId, $name, $price, $imagePath, $detail, $number, $category, $state);
 
 $finishPage = "managerFastFood.php";
-// if ($addType == 2) {
-// 	$finishPage = "managerHotPot.php";
-// } elseif ($addType == 3) {
-// 	$finishPage = "managerDrinks.php";
-// } elseif ($addType == 4) {
-// 	$finishPage = "managerOthers.php";
-// }
+if ($addType == 2) {
+	$finishPage = "managerHotPot.php";
+} elseif ($addType == 3) {
+	$finishPage = "managerDrinks.php";
+} elseif ($addType == 4) {
+	$finishPage = "managerOthers.php";
+}
 
  ?>
 	<p>添加成功</p>
