064b651cf3de2a9d2c5df23681633ab05d52c5bb
ZhangYaoYuan <Zhang_Yung@126.com>
2018-04-11 17:10:55 +0800

    update

--- manager/category/managerAddProductFinish.php
+++ manager/category/managerAddProductFinish.php
@@ -16,47 +16,50 @@
 	$number = $_POST["number"];
 	$status = $_POST["status"];
 	$file = $_FILES["file"];
+	$imagePath = NULL;
 
-	// 允许上传的图片后缀
-	$allowedExts = array("gif", "jpeg", "jpg", "png");
-	$temp = explode(".", $file["name"]);
-	$extension = end($temp);     // 获取文件后缀名
-	echo "文件临时存储的位置: " . $file["tmp_name"] . "<br>";
+	if ($file != NULL) {
+		// 允许上传的图片后缀
+		$allowedExts = array("gif", "jpeg", "jpg", "png");
+		$temp = explode(".", $file["name"]);
+		$extension = end($temp);     // 获取文件后缀名
+		echo "文件临时存储的位置: " . $file["tmp_name"] . "<br>";
 
-	$imagePath = $uploadImageFileFoldPath . intval(time()) . md5($file["name"]) . "." . $extension;
-	$uploadPath = $_SERVER['DOCUMENT_ROOT'] . $imagePath;
-	echo $uploadPath . "<br>";
-	if ((($file["type"] == "image/gif")
-	|| ($file["type"] == "image/jpeg")
-	|| ($file["type"] == "image/jpg")
-	|| ($file["type"] == "image/pjpeg")
-	|| ($file["type"] == "image/x-png")
-	|| ($file["type"] == "image/png"))
-	&& ($file["size"] < 307200)
-	&& in_array($extension, $allowedExts))
-	{
-	    if ($file["error"] > 0)
-	    {
-	        echo "上传文件错误：: " . $file["error"] . "<br>";
-	    }
-	    else
-	    {   
-	        if (file_exists($uploadPath))
-	        {
-	            die($uploadPath . " 文件已经存在。如果多次尝试都有此问题，请联系管理员");
-	        }
-	        else
-	        {
-	            // 如果 upload 目录不存在该文件则将文件上传到 upload 目录下
-	            $createImage = move_uploaded_file($file["tmp_name"], $uploadPath);
-	            if ($createImage == FALSE) {
-	            	die("创建图片失败， 请联系管理员");
-	            }
-	        }
-	    }
-	} else {
-		echo "type:" . $file["type"] . "<br>" . $file["size"] . "<br>" . $extension;
-	    die ("非法的文件格式, 文件只支持png,jpg,jpeg,文件要少于200kb");
+		$imagePath = $uploadImageFileFoldPath . intval(time()) . md5($file["name"]) . "." . $extension;
+		$uploadPath = $_SERVER['DOCUMENT_ROOT'] . $imagePath;
+		echo $uploadPath . "<br>";
+		if ((($file["type"] == "image/gif")
+		|| ($file["type"] == "image/jpeg")
+		|| ($file["type"] == "image/jpg")
+		|| ($file["type"] == "image/pjpeg")
+		|| ($file["type"] == "image/x-png")
+		|| ($file["type"] == "image/png"))
+		&& ($file["size"] < 307200)
+		&& in_array($extension, $allowedExts))
+		{
+		    if ($file["error"] > 0)
+		    {
+		        echo "上传文件错误：: " . $file["error"] . "<br>";
+		    }
+		    else
+		    {   
+		        if (file_exists($uploadPath))
+		        {
+		            die($uploadPath . " 文件已经存在。如果多次尝试都有此问题，请联系管理员");
+		        }
+		        else
+		        {
+		            // 如果 upload 目录不存在该文件则将文件上传到 upload 目录下
+		            $createImage = move_uploaded_file($file["tmp_name"], $uploadPath);
+		            if ($createImage == FALSE) {
+		            	die("创建图片失败， 请联系管理员");
+		            }
+		        }
+		    }
+		} else {
+			echo "type:" . $file["type"] . "<br>" . $file["size"] . "<br>" . $extension;
+		    die ("非法的文件格式, 文件只支持png,jpg,jpeg,文件要少于200kb");
+		}
 	}
 
 	// 1:快餐fastFood 2:火锅hotPot 3:饮料drinks 4:其他others
