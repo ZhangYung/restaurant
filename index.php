07f21e13c127623ed3498f43e0dde2f9050451e1
ZhangYaoYuan <Zhang_Yung@126.com>
2018-04-12 10:36:59 +0800

    update

--- index.php
+++ index.php
@@ -11,13 +11,14 @@
 <title>水云居</title>
 </head>
 
-<frameset rows="80%, *">
+<!-- <frameset rows="80%, *" frameBorder="1"> -->
 <frameset cols="20%, *" frameBorder="1">
 	<frame name="selectContent" src="category/homePageLeft.php" noresize="noresize"></frame>
 	<frame name="mainContent" src="category/fastFood.php?category=1"></frame>
 </frameset>
-	<frame name="priceContent" src="priceBottom.php"></frame>
-</frameset>
+<!-- 
+	<frame name="priceContent" src="category/priceBottom.php"></frame>
+</frameset> -->
 </html>
 
 
