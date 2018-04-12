80632a836aa4534801571f1cd4f4952327ca94f7
ZhangYaoYuan <Zhang_Yung@126.com>
2018-04-12 11:46:14 +0800

    update

--- category/fastFood.php
+++ category/fastFood.php
@@ -35,13 +35,13 @@
 				echo "剩余：" . $model->number . "<br>";
 				echo "</td>";
 
-				// echo "<td>";
+				echo "<td>";
 				// echo "<form action=\"managerAddProduct.php\" method=\"get\" id=\"form" . $i . "\">";
 				// echo "<input type=\"hidden\" name=\"addType\" value=\"" . $currentCategory . "\">";
 				// echo "<input type=\"hidden\" name=\"changeProductId\" value=\"" . $model->productId . "\">";
-				// echo "<input class=\"elementButton\" type=\"submit\" name=\"submit\" value=\"添加\">";
+				echo "<input class=\"elementButton\" type=\"button\" on_click=\"clickAdd()\" value=\"添加\">";
 				// echo "</form>";
-				// echo "</td>";
+				echo "</td>";
 
 				echo " </tr>";
 			}
--- index.php
+++ index.php
@@ -7,17 +7,44 @@
 <html>
 <head>
 <meta charset="utf-8">
-<link rel="stylesheet" type="text/css" href="stylesheet/homepage.css" />
+<link rel="stylesheet" type="text/css" href="stylesheet/homepage.css"/>
 <title>水云居</title>
 </head>
 
-<frameset rows="80%, 20%">
+<script type="text/javascript">
+	function clickChooseProductFunction(category) {
+
+	}
+</script>
+<body>
+	<link rel="stylesheet" type="text/css" href="">
+
+
+<div id="container" style="width: 100%">
+	<div id="header" style="background-color:#FFA500;">
+		<h1 style="text-align:center">水云居</h1>
+	</div>
+
+	<div id="menu">
+		<button onclick="clickChooseProductFunction(1)">美味快餐</button>
+		<button onclick="clickChooseProductFunction(2)">火锅</button>
+		<button onclick="clickChooseProductFunction(3)">饮料</button>
+		<button onclick="clickChooseProductFunction(4)">其他</button>
+		
+	</div>
+
+</div>
+<!-- 
+<frameset rows="85%, 15%">
 	<frameset cols="20%, 80%">
-		<frame name="selectContent" src="category/homePageLeft.php" noresize="noresize"/>
+		<frame name="selectContent" src="category/homePageLeft.php"/>
 		<frame name="mainContent" src="category/fastFood.php?category=1"/>
 	</frameset>
 	<frame name="priceContent" src="category/priceBottom.php"/>
-</frameset>
+ </frameset>
+-->
+
+</body>
 </html>
 
 
