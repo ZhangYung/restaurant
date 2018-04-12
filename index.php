0e944ee7c24802b4438b249889cd3436a8e57273
ZhangYaoYuan <Zhang_Yung@126.com>
2018-04-12 14:43:34 +0800

    update

--- index.php
+++ index.php
@@ -18,50 +18,35 @@
 </script>
 <body>
 
-<div id="container" style="width: 100%; position:absolute;top:0px;bottom:0px;">
-	<div id="header" style="background-color:#FFA500; height: 10%; width: 100%;">
+<div id="container" style="width:100%;position:absolute;top:0px;bottom:0px;">
+	<div id="header" style="background-color:#FFA500; height: 80px; width: 100%;">
 		<h1 style="text-align:center">水云居</h1>
 	</div>
 
-	<div id="menu" style="width: 20%; float: left; height: 80%;">
+	<div id="menu" style="width:20%;float:left;height:90%;">
 		<button class="categorySelect" onclick="clickChooseProductFunction(1)">美味快餐</button> <br><br>
 		<button class="categorySelect" onclick="clickChooseProductFunction(2)">火锅</button> <br><br>
 		<button class="categorySelect" onclick="clickChooseProductFunction(3)">饮料</button> <br><br>
 		<button class="categorySelect" onclick="clickChooseProductFunction(4)">其他</button> <br><br>
 	</div>
 
-	<div id="content" style="width: 80%; float: right; height: 60%;">
+	<div id="content" style="width:80%;float:right;height:70%;">
 		<?php 
 			require "category/fastFood.php";
 		?>
 	</div>
 
-	<div id="content" style="width: 80%; float: right; height: 20%;">
+	<div id="content" style="width:80%;float:right;height:20%;">
 		<?php 
 			require "category/priceBottom.php";
 		?>
 	</div>
 
-	<div id="footer" style="text-align: center; height: 10%; width: 100%; float: left;">
+	<div id="footer" style="text-align:center;height:10%;width:100%;float:left;">
 		版权 ©2017-2018美美餐厅水云居
 	</div>
 
 </div>
-
-<script type="text/javascript">
-	function clickAdd() {
-
-	}
-</script>
-<!-- 
-<frameset rows="85%, 15%">
-	<frameset cols="20%, 80%">
-		<frame name="selectContent" src="category/homePageLeft.php"/>
-		<frame name="mainContent" src="category/fastFood.php?category=1"/>
-	</frameset>
-	<frame name="priceContent" src="category/priceBottom.php"/>
- </frameset>
--->
 
 </body>
 </html>
