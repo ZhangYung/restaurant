df055dcab62a4b414c1ead437b45bd11a998e511
ZhangYaoYuan <Zhang_Yung@126.com>
2018-04-12 10:23:59 +0800

    update

---
+++ category/priceBottom.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="../stylesheet/homepage.css">
+	<title></title>
+</head>
+<body>
+	<table>
+		<tr>
+			<td>
+				price
+			</td>
+			<td>
+				确定
+			</td>
+		</tr>
+	</table>
+</body>
+</html>
--- index.php
+++ index.php
@@ -1,3 +1,8 @@
+<?php
+	session_start();
+	$_SESSION['purchaseProduct']=array();
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -6,9 +11,12 @@
 <title>水云居</title>
 </head>
 
+<frameset rows="80%, *">
 <frameset cols="20%, *" frameBorder="1">
 	<frame name="selectContent" src="category/homePageLeft.php" noresize="noresize"></frame>
 	<frame name="mainContent" src="category/fastFood.php?category=1"></frame>
+</frameset>
+	<frame name="priceContent" src="priceBottom.php"></frame>
 </frameset>
 </html>
 
