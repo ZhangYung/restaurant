8d76e84c06e6fb55b1b24f1c56bdc1415da75389
ZhangYaoYuan <Zhang_Yung@126.com>
2018-04-13 17:19:32 +0800

    update

--- category/createOrder.php
+++ category/createOrder.php
@@ -93,6 +93,7 @@ if ($_GET["createOrder"] != NULL) {
 		$sqlProductPriceArray[$i] = $model->price;
 		$sqlProductNumberArray[$i] = $productIdNumDic[$model->productId];
 	}
+	echo $_SESSION['eatSeatNum'];
 	$success = addOrEditOrder(NULL, $_SESSION['eatTableNum'], $_SESSION['eatSeatNum'], intval(time()), $priceSum, implode(",", $sqlProductIdArray), implode(",", $sqlProductNameArray), implode(",", $sqlProductPriceArray), implode(",", $sqlProductNumberArray), 1);
 	if ($success) {
 		$_SESSION['purchaseProducts'] = NULL;
