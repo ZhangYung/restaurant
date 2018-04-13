3b1e5a68cac61842f07795c706ad1f6576c86a7e
ZhangYaoYuan <Zhang_Yung@126.com>
2018-04-13 11:33:23 +0800

    update

--- category/createOrder.php
+++ category/createOrder.php
@@ -96,7 +96,7 @@ if ($_POST["createOrder"] != NULL) {
 	$success = addOrEditOrder(NULL, $_SESSION['eatTableNum'], intval(time()), $priceSum, implode(",", $sqlProductIdArray), implode(",", $$sqlProductNameArray), implode(",", $sqlProductPriceArray), implode(",", $sqlProductNumberArray), 1);
 	if ($success) {
 		$_SESSION['purchaseProducts'] = NULL;
-		echo "<script language=\"javascript\" >alert(\"下单成功\"); window.location.go(-2);</script>";
+		echo "<script language=\"javascript\" >alert(\"下单成功\"); history.go(-2); location.reload();</script>";
 	} else {
 		echo "<script language=\"javascript\" >alert(\"下单失败，请联系店主手动下单\"); history.go(-2);</script>";
 	}
