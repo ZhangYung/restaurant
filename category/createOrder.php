6e3e6a0d920fb82cdca0f619c2201e54cd35f44f
ZhangYaoYuan <Zhang_Yung@126.com>
2018-04-13 15:41:50 +0800

    update

--- category/createOrder.php
+++ category/createOrder.php
@@ -64,7 +64,7 @@ echo "</table>";
 	echo "<br>";
 	// echo "<form method=\"post\" id=\"form1\">";
 	// echo "<input type=\"hidden\" name=\"createOrder\" value=\"" . "createOrder" . "\">";
-	echo "<input class=\"elementButton\" type=\"button\" name=\"submit\" value=\"确定下单\" onClick=\"javascript:location.replace('createOrder.php?createOrder=\"createOrder\"')\">";
+	echo "<button class=\"elementButton\" onclick=\"javascript:location.replace('createOrder.php?createOrder=\"createOrder\"')\">下单</button>";
 	// echo "</form>";
 	echo "<br>";
 	echo "<br>";
