2327c2b1ccab75e71b535d63eb2262eb7bf84cea
ZhangYaoYuan <Zhang_Yung@126.com>
2018-04-19 18:21:48 +0800

    update

--- category/homepageLeft.php
+++ category/homepageLeft.php
@@ -1,11 +1,11 @@
 <!DOCTYPE html>
 <html>
 <head>
-<link rel="stylesheet" type="text/css" href=../"stylesheet/homepage.css"/>
+<link rel="stylesheet" type="text/css" href="../stylesheet/homepage.css"/>
 </head>
 <body>
 		<ul>
-			<li ><a class="selected" href="category/fastFood.php?category=1" target="mainContent" style="text-decoration:none;">美味快餐</a></li><br>
+			<li ><a class="selected" href="fastFood.php?category=1" target="mainContent" style="text-decoration:none;">美味快餐</a></li><br>
 			<li><a href="fastFood.php?category=2" target="mainContent" style="text-decoration:none;">火锅</a></li><br>
 			<li><a href="fastFood.php?category=3" target="mainContent" style="text-decoration:none;">饮料</a></li><br>
 			<li><a href="fastFood.php?category=4" target="mainContent" style="text-decoration:none;">其他</a></li><br>
