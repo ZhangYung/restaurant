cc2659e145249ca7cb54f6c0dbbd22fe397f2d3e
ZhangYaoYuan <Zhang_Yung@126.com>
2018-04-28 11:45:55 +0800

    update

--- category/homepageLeft.php
+++ category/homepageLeft.php
@@ -19,13 +19,14 @@ $(document).ready(function()
 <body>
 	<?php 
 	$currentCategory = $_GET["selectedCategory"]; 
+	echo "currentCategory:" . $currentCategory;
 	 ?>
 		<ul>
-			<li ><a <?php if($currentCategory == "1") { echo " class=\"selected\" ";} ?> href="fastFood.php?category=1" target="mainContent" style="text-decoration:none;">美味快餐</a></li><br>
-			<li><a <?php if($currentCategory == "2") { echo " class=\"selected\" ";} ?> href="fastFood.php?category=2" target="mainContent" style="text-decoration:none;">火锅</a></li><br>
-			<li><a <?php if($currentCategory == "3") { echo " class=\"selected\" ";} ?> href="fastFood.php?category=3" target="mainContent" style="text-decoration:none;">饮料</a></li><br>
-			<li><a <?php if($currentCategory == "4") { echo " class=\"selected\" ";} ?> href="fastFood.php?category=4" target="mainContent" style="text-decoration:none;">其他</a></li><br>
-			<li><a <?php if($currentCategory == "5") { echo " class=\"selected\" ";} ?> href="orderList.php" target="mainContent" style="text-decoration:none;">已下订单</a></li><br>
+			<li ><a <?php if($currentCategory == 1) { echo " class=\"selected\" ";} ?> href="fastFood.php?category=1" target="mainContent" style="text-decoration:none;">美味快餐</a></li><br>
+			<li><a <?php if($currentCategory == 2) { echo " class=\"selected\" ";} ?> href="fastFood.php?category=2" target="mainContent" style="text-decoration:none;">火锅</a></li><br>
+			<li><a <?php if($currentCategory == 3) { echo " class=\"selected\" ";} ?> href="fastFood.php?category=3" target="mainContent" style="text-decoration:none;">饮料</a></li><br>
+			<li><a <?php if($currentCategory == 4) { echo " class=\"selected\" ";} ?> href="fastFood.php?category=4" target="mainContent" style="text-decoration:none;">其他</a></li><br>
+			<li><a <?php if($currentCategory == 5) { echo " class=\"selected\" ";} ?> href="orderList.php" target="mainContent" style="text-decoration:none;">已下订单</a></li><br>
 		</ul>
 </body>
 </html>
