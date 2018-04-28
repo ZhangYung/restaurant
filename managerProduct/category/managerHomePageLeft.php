8270cbedb7e0a18b8e6fbbde122c963eed4600dd
ZhangYaoYuan <Zhang_Yung@126.com>
2018-04-28 15:52:32 +0800

    update

--- managerProduct/category/managerHomePageLeft.php
+++ managerProduct/category/managerHomePageLeft.php
@@ -3,7 +3,7 @@
 <head>
 	<!-- <link rel="stylesheet" type="text/css" href="../stylesheet/managerHomepage.css"> -->
 	<link rel="stylesheet" type="text/css" href="../../stylesheet/homepage.css"/>
-<script type="text/javascript" src="../stylesheet/jquery-3.3.1.min.js"></script>
+<script type="text/javascript" src="../../stylesheet/jquery-3.3.1.min.js"></script>
 <script type="text/javascript">
 $(document).ready(function()
   {
