75cb082f58dc30eedd63a691983bde660a627dda
ZhangYaoYuan <Zhang_Yung@126.com>
2018-04-28 11:18:14 +0800

    update

--- category/homepageLeft.php
+++ category/homepageLeft.php
@@ -18,11 +18,11 @@ $(document).ready(function()
 </head>
 <body>
 		<ul>
-			<li ><a <?php $_SESSION['categoryShow'] == "1" ? (echo " class=\"selected\" ";) : (echo "";); ?> href="fastFood.php?category=1" target="mainContent" style="text-decoration:none;">美味快餐</a></li><br>
-			<li><a <?php $_SESSION['categoryShow'] == "2" ? (echo " class=\"selected\" ";) : (echo "";); ?> href="fastFood.php?category=2" target="mainContent" style="text-decoration:none;">火锅</a></li><br>
-			<li><a <?php $_SESSION['categoryShow'] == "3" ? (echo " class=\"selected\" ";) : (echo "";); ?> href="fastFood.php?category=3" target="mainContent" style="text-decoration:none;">饮料</a></li><br>
-			<li><a <?php $_SESSION['categoryShow'] == "4" ? (echo " class=\"selected\" ";) : (echo "";); ?> href="fastFood.php?category=4" target="mainContent" style="text-decoration:none;">其他</a></li><br>
-			<li><a <?php $_SESSION['categoryShow'] == "5" ? (echo " class=\"selected\" ";) : (echo "";); ?> href="orderList.php" target="mainContent" style="text-decoration:none;">已下订单</a></li><br>
+			<li ><a <?php if($_SESSION['categoryShow'] == "1") { echo " class=\"selected\" ";} ?> href="fastFood.php?category=1" target="mainContent" style="text-decoration:none;">美味快餐</a></li><br>
+			<li><a <?php if($_SESSION['categoryShow'] == "2") { echo " class=\"selected\" ";} ?> href="fastFood.php?category=2" target="mainContent" style="text-decoration:none;">火锅</a></li><br>
+			<li><a <?php if($_SESSION['categoryShow'] == "3") { echo " class=\"selected\" ";} ?> href="fastFood.php?category=3" target="mainContent" style="text-decoration:none;">饮料</a></li><br>
+			<li><a <?php if($_SESSION['categoryShow'] == "4") { echo " class=\"selected\" ";} ?> href="fastFood.php?category=4" target="mainContent" style="text-decoration:none;">其他</a></li><br>
+			<li><a <?php if($_SESSION['categoryShow'] == "5") { echo " class=\"selected\" ";} ?> href="orderList.php" target="mainContent" style="text-decoration:none;">已下订单</a></li><br>
 		</ul>
 </body>
 </html>
